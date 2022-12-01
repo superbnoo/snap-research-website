dbe803e38093accaf77702a5c8c23754d5bd9b35
Guru Krishnan <guru.krishnan@outlook.com>
2022-12-01 10:47:50 -0800

    Squashed commit of the following:
    commit b81aaae3485b07cb3848dc2d01a36eb2e16bc1ca
    Author: Guru Krishnan <guru.krishnan@outlook.com>
    Date:   Thu Dec 1 10:45:52 2022 -0800
    
        Added padding
    
    commit ba1ffad50062625f2ec16b5cd2f16e64468c8ae0
    Author: Guru Krishnan <guru.krishnan@outlook.com>
    Date:   Wed Nov 30 17:13:47 2022 -0800
    
        added fellowship winners

---
+++ js/fellowship-winners-lib.js
@@ -0,0 +1,138 @@
+const fellowshipWinnersList = [
+  {
+    id: 0,
+    name: "Dejia Xu",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Dejia-Xu.jpg",
+    description: `
+    <p>Dejia-Xu is from The University of Texas at Austin.</p>
+    `,
+    university: "The University of Texas at Austin",
+    url: "https://ir1d.github.io/"
+  },
+  {
+    id: 1,
+    name: "Riku Arakawa",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Riku-Arakawa.jpg",
+    description: `
+    <p>Riku Arakawa is from Carnegie Mellon University.</p>.
+    `,
+    university: "Carnegie Mellon University",
+    url: "https://rikky0611.github.io/"
+  },
+  {
+    id: 2,
+    name: "Ruojin Cai",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Ruojin-Cai.jpg",
+    description: `
+    <p>Ruojin Cai is from Cornell University.</p>.
+    `,
+    university: "Cornell University",
+    url: "https://www.cs.cornell.edu/~ruojin/"
+  },
+  {
+    id: 3,
+    name: "Liang Shi",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Liang-Shi.jpg",
+    description: `
+    <p>Liang Shi is from Massachusetts Institute of Technology.</p>.
+    `,
+    university: "Massachusetts Institute of Technology",
+    url: "https://people.csail.mit.edu/liangs/"
+  },
+  {
+    id: 4,
+    name: "Gaurav Verma",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Gaurav-Verma.jpg",
+    description: `
+    <p>Gaurav Verma is from Georgia Institute of Technology.</p>.
+    `,
+    university: "Georgia Institute of Technology",
+    url: "https://gaurav22verma.github.io/"
+  },
+  {
+    id: 5,
+    name: "Julie Jiang",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Julie-Jiang.webp",
+    description: `
+    <p>Julie Jiang is from University of Southern California.</p>.
+    `,
+    university: "University of Southern California",
+    url: "https://julie-jiang.github.io/"
+  },
+  {
+    id: 6,
+    name: "Wei Jin",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Wei-Jin.jpg",
+    description: `
+    <p>Wei Jin is from Michigan State University.</p>.
+    `,
+    university: "Michigan State University",
+    url: "http://cse.msu.edu/~jinwei2/"
+  },
+  {
+    id: 7,
+    name: "Zhichun Guo",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Zhichun-Guo.webp",
+    description: `
+    <p>Zhichun Guo is from University of Notre Dame.</p>.
+    `,
+    university: "University of Notre Dame",
+    url: "https://zguo.io/"
+  },
+  {
+    id: 8,
+    name: "Eric Chan",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Eric-Ryan-Chan.jpg",
+    description: `
+    <p>Eric Chan is from Stanford University.</p>.
+    `,
+    university: "Stanford University",
+    url: "https://ericryanchan.github.io"
+  },
+  {
+    id: 9,
+    name: "Mikaela Angelina Uy",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Mikaela-Angelina-Uy.webp",
+    description: `
+    <p>Mikaela Angelina Uy is from Stanford University.</p>.
+    `,
+    university: "Stanford University",
+    url: "https://mikacuy.github.io"
+  },
+  {
+    id: 10,
+    name: "Sheng-Yu Wang",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Sheng-Yu-Wang.jpg",
+    description: `
+    <p>Sheng-Yu Wang is from Carnegie Mellon University.</p>.
+    `,
+    university: "Carnegie Mellon University",
+    url: "https://peterwang512.github.io/"
+  },
+  {
+    id: 11,
+    name: "Weihao Yu",
+    backUrl: "fellowships.html",
+    imageUrl: "img-for-fellowships/Weihao-Yu.jpg",
+    description: `
+    <p>Weihao Yu is from National University of Singapore.</p>.
+    `,
+    university: "National University of Singapore",
+    url: "https://whyu.me"
+  },
+];
+
+function getFellowshipWinners() {
+  return fellowshipWinnersList;
+}
