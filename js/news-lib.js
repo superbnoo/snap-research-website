4bf8b7d52f2a7a57dd38958c8220097f11fd9573
Noo Tran <superbnoo@gmail.com>
2023-07-08 15:57:02 +0700

    Add rethinking graph news

--- js/news-lib.js
+++ js/news-lib.js
@@ -8,11 +8,25 @@ const newsList = [
       "Snap Research creates a new way to digitize and render assets for Augmented Reality",
     descriptionShort:
       "Snap Research presents NeRoic (Neural Object Capture and Rendering from Online Image Collections), a new method that explores how to digitize and render objects more easily than ever before, at SIGGRAPH.",
-    description: "../news/detail/neroic-2022.html",
+    description: "../news/detail/rethinking-gnns-2023.html",
     metaTitle:
       "Snap Research creates a new way to digitize and render assets for Augmented Reality",
     metaDescription: "Snap Research presents NeRoic (Neural Object Capture and Rendering from Online Image Collections), a new method that explores how to digitize and render objects more easily than ever before, at SIGGRAPH.",
   },
+  {
+    id: 1,
+    date: "July 8, 2023",
+    link: "../news.html",
+    slug: "rethink-graph-2023",
+    title:
+      "Rethinking the role of Graph Neural Networks in Knowledge Graph Completion",
+    descriptionShort:
+      "Knowledge Graphs (KGs) are an efficient method to store relational and typically factual data (e.g. Freebase, NELL, and ConceptNet). They are used for various applications including search, recommendation, and question-answering systems.",
+    description: "../news/detail/rethinking-gnns-2023.html",
+    metaTitle:
+      "Rethinking the role of Graph Neural Networks in Knowledge Graph Completion",
+    metaDescription: "Knowledge Graphs (KGs) are an efficient method to store relational and typically factual data (e.g. Freebase, NELL, and ConceptNet). They are used for various applications including search, recommendation, and question-answering systems.",
+  }
 ];
 
 function getNews() {
