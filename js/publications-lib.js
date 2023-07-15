d17a28dd4f3c30a68accd26866a024f9c44dae6a
Noo Tran <superbnoo@gmail.com>
2023-07-15 08:40:27 +0700

    home publications - show 5 random papers from latest 25 papers

--- js/publications-lib.js
+++ js/publications-lib.js
@@ -3083,6 +3083,20 @@ publicationsList.forEach((publication) => {
 
 publicationsList.sort((a, b) => new Date(b.date) - new Date(a.date));
 
+function get5Latest25Papers() {
+  if (publicationsList.length < 25) return [];
+  
+  let numbers = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
+    20, 21, 22, 23, 24
+  ];
+  const shuffled = numbers.sort(() => 0.5 - Math.random());
+  const idx = shuffled.slice(0, 5).sort((a, b) => a - b);
+
+  return idx.map((i) => publicationsList[i].id);
+}
+
 function getPublicationsByIDs(id, path, noSort) {
   if (!id.length) return [];
   let result = [];
